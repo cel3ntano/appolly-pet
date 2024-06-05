6462a9eb27cac9c60d3cb9e0af760c5bf6da8975
cel3ntano <160436964+cel3ntano@users.noreply.github.com>
2024-06-05 20:49:28 +0000

    Deploying to gh-pages from @ cel3ntano/appolly-pet@7a8421fcb6cdaa07293b792daa4c73977468ad7c 🚀

--- commonHelpers3.js
+++ commonHelpers3.js
@@ -1,2 +1,2 @@
-import"./assets/main-79308c46.js";import{S as e,N as o,P as a}from"./assets/vendor-6b8f44a5.js";new e(".swiper",{modules:[o,a],spaceBetween:50,effect:"coverflow",coverflowEffect:{rotate:30,slideShadows:!1},loop:!0,pagination:{el:".swiper-pagination"}});console.log("do pobachennya");
+import"./assets/main-48c44c0c.js";import{S as e,N as o,P as a}from"./assets/vendor-6b8f44a5.js";new e(".swiper",{modules:[o,a],spaceBetween:50,effect:"coverflow",coverflowEffect:{rotate:30,slideShadows:!1},loop:!0,pagination:{el:".swiper-pagination"}});console.log("do pobachennya");
 //# sourceMappingURL=commonHelpers3.js.map
